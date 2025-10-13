956bb540de6c0d212b21eaca58349c030874f578
HNikolov04 <h.nikolov2004@gmail.com>
2025-10-13 15:49:35 +0300

    Initial Skeleton Structure Commit

---
+++ server/cinemetro/src/main/java/com/cineworld/cinemetro/CineMetroApplication.java
@@ -0,0 +1,13 @@
+package com.cineworld.cinemetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineMetroApplication.class, args);
+	}
+
+}
---
+++ server/cinemetro/src/test/java/com/cineworld/cinemetro/CineMetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineworld.cinemetro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineMetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
