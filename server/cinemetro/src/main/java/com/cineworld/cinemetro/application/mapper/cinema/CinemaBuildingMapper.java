863eed5049fbeb096715818695bc1787f36d0981
Hristo Nikolov <h.nikolov2004@gmail.com>
2025-11-06 13:13:04 +0200

    Implement CityService and CinemaBuildingService with corresponding mappers

---
+++ server/cinemetro/src/main/java/com/cineworld/cinemetro/application/mapper/cinema/CinemaBuildingMapper.java
@@ -0,0 +1,27 @@
+package com.cineworld.cinemetro.application.mapper.cinema;
+
+import com.cineworld.cinemetro.application.dto.cinema.cinemabuilding.response.GetAllCinemaBuildingsResponseDto;
+import com.cineworld.cinemetro.application.dto.cinema.cinemabuilding.response.GetCinemaBuildingResponseDto;
+import com.cineworld.cinemetro.domain.model.cinema.CinemaBuilding;
+
+public class CinemaBuildingMapper {
+
+    public static GetCinemaBuildingResponseDto toGetCinemaBuildingDto(CinemaBuilding b) {
+        return new GetCinemaBuildingResponseDto(
+                b.getId(),
+                b.getName(),
+                b.getAddress(),
+                b.getCity() != null ? b.getCity().getId() : null
+        );
+    }
+
+    public static GetAllCinemaBuildingsResponseDto toGetAllDto(CinemaBuilding b) {
+        return new GetAllCinemaBuildingsResponseDto(
+                b.getId(),
+                b.getName(),
+                b.getAddress()
+        );
+    }
+}
+
+
